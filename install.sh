f7d5c4cf3070b9eb093646e6bf157ffb0d165e39
Angelillo15 <43421778+Angelillo15@users.noreply.github.com>
2022-07-26 21:15:59 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -10,14 +10,25 @@ installTheme(){
     tar -cvf MinecraftPurpleThemebackup.tar.gz pterodactyl
     echo "Installing theme..."
     cd /var/www/pterodactyl
+    rm -r MinecraftPurpleTheme
     git clone https://github.com/Angelillo15/MinecraftPurpleTheme.git
     cd MinecraftPurpleTheme
     rm /var/www/pterodactyl/resources/scripts/index.tsx
     mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
     mv MinecraftPurpleTheme.css /var/www/pterodactyl/resources/scripts/MinecraftPurpleTheme.css
     cd /var/www/pterodactyl
-    sudo yarn build:production
+
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    apt install -y nodejs
+
+    npm i -g yarn
+    yarn
+
+    cd /var/www/pterodactyl
+    yarn build:production
     sudo php artisan optimize:clear
+
+
 }
 
 
